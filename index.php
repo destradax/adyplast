f98890343c9924d93cee814503cad5eae30e8893
destradax@gmail.com <destradax@gmail.com>
2015-02-19 20:35:22 -0500

    Fixed indentation. Added custom styles file

--- index.php
+++ index.php
@@ -1,161 +1,182 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="adyplast">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="adyplast">
 
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="css/adyplast.css">
 
-    <script src="js/jquery-2.1.3.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/docs.min.js"></script>
+	<script src="js/jquery-2.1.3.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/docs.min.js"></script>
 
-    <title>Adyplast.co</title>
+	<title>Adyplast.co</title>
 
-    <link href="css/carousel.css" rel="stylesheet">
-  </head>
+	<link href="css/carousel.css" rel="stylesheet">
+</head>
 
-  <body>
-    <div class="navbar-wrapper">
-      <div class="container">
+<body>
+	<div class="navbar-wrapper">
+		<div class="container">
 
-        <nav class="navbar navbar-inverse navbar-static-top">
-          <div class="container">
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="#">Adyplast</a>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse">
-              <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Inicio</a></li>
-                <li><a href="#quienes-somos">Quienes Somos</a></li>
-                <li><a href="#servicios">Servicios</a></li>
-                <li><a href="#productos">Productos</a></li>
-                <li><a href="#contacto">Contacto</a></li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+			<nav class="navbar navbar-inverse navbar-static-top">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Adyplast</a>
+					</div>
+					<div id="navbar" class="navbar-collapse collapse">
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="#">Inicio</a></li>
+							<li><a href="#quienes-somos">Quienes Somos</a></li>
+							<li><a href="#servicios">Servicios</a></li>
+							<li><a href="#productos">Productos</a></li>
+							<li><a href="#contacto">Contacto</a></li>
+							<li><a href="#escribenos">Escribenos</a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
 
-      </div>
-    </div>
-
-
-    <!-- Carousel
-    ================================================== -->
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      </ol>
-      <div class="carousel-inner" role="listbox">
-        <div class="item active">
-          <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>Adyplast</h1>
-              <p>Especialistas en lo mejor y lo novedoso.</p>
-              <p>&nbsp;</p>
-              <p><a class="btn btn-lg btn-primary" href="mailto:adyplast@gmail.com" role="button">Escríbenos!</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div><!-- /.carousel -->
+		</div>
+	</div>
 
 
-    <!-- Marketing messaging and featurettes
-    ================================================== -->
-    <!-- Wrap the rest of the page in another container to center all the content. -->
+	<!-- Carousel
+	================================================== -->
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		</ol>
+		<div class="carousel-inner" role="listbox">
+			<div class="item active">
+				<img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
+				<div class="container">
+					<div class="carousel-caption">
+						<h1>Adyplast</h1>
+						<p>Especialistas en lo mejor y lo novedoso.</p>
+						<p>&nbsp;</p>
+						<p><a class="btn btn-lg btn-primary" href="#escribenos" role="button">Escríbenos!</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div><!-- /.carousel -->
 
-    <div class="container marketing">
 
-      <div class="row featurette">
-        <div class="col-md-12">
-          <h2 id="quienes-somos" class="featurette-heading">Quienes Somos</h2>
-          <p class="lead">
-            Somos una empresa con mas de 30 años en el mercado, poniendo al servicio de
-            nuestros clientes tecnología propia, además de aportar ideas nuevas para la
-            solución de sus necesidades en la fabricación de bolsas plásticas y similares.
-          </p>
-          <p class="lead">
-            Trabajamos con honestidad y compromiso para nuestro bienestar y el de nuestros
-            clientes.
-          </p>
-        </div>
-      </div>
+	<!-- Marketing messaging and featurettes
+	================================================== -->
+	<!-- Wrap the rest of the page in another container to center all the content. -->
 
-      <hr class="featurette-divider">
+	<div class="container marketing">
 
-      <div class="row featurette">
-        <div class="col-md-12">
-          <h2 id="servicios" class="featurette-heading">Nuestros Servicios</h2>
-          <ul class="lead">
-            <li>Prestamos el servicio de sellado en alta y baja densidad, en materiales como polietileno, polipropileno, materiales termoencogibles, entre otros.</li>
-            <li>Confeccionamos bolsas impresas o transparentes, con adhesivo, con solapa interior o exterior, solapa volada o exterior invertida, y fuelle de fondo.</li>
-            <li>Sellamos desde 3 hasta 120 cm de largo, en sello de fondo o sello lateral.</li>
-            <li>Sellamos bolsas en alta densidad con fuelles laterales hasta calibre 3.</li>
-            <li>A sus rollos planos le hacemos fuelle abierto para troquel riñón. Tambien le hacemos el sello falso con el nombre de su empresa.</li>
-            <li>Sellado para bolsas a prueba de líquidos con medidas exactas (en milímetros).</li>
-            <li>Refilamos rollos planos y con solapa.</li>
-            <li>Reembobinamos para que sus rollos den el máximo rendimiento.</li>
-          </ul>
-        </div>
-      </div>
+		<div class="row featurette">
+			<div class="col-md-12">
+				<h2 id="quienes-somos" class="featurette-heading">Quienes Somos</h2>
+				<p class="lead">
+					Somos una empresa con mas de 30 años en el mercado, poniendo al servicio de
+					nuestros clientes tecnología propia, además de aportar ideas nuevas para la
+					solución de sus necesidades en la fabricación de bolsas plásticas y similares.
+				</p>
+				<p class="lead">
+					Trabajamos con honestidad y compromiso para nuestro bienestar y el de nuestros
+					clientes.
+				</p>
+			</div>
+		</div>
 
-      <hr class="featurette-divider">
+		<hr class="featurette-divider">
 
-      <div class="row featurette">
-        <div class="col-md-12">
-          <h2 id="productos" class="featurette-heading">Nuestros Productos</h2>
-          <p class="lead">
-            Confeccionamos bates publicitarios (proporcionamos la válvula).
-          </p>
-          <p class="lead">
-            Somos inventores de la bolsa separadora de residuos en 2 y 3 compartimientos.
-          </p>
-        </div>
-      </div>
+		<div class="row featurette">
+			<div class="col-md-12">
+				<h2 id="servicios" class="featurette-heading">Nuestros Servicios</h2>
+				<ul class="lead">
+					<li>Prestamos el servicio de sellado en alta y baja densidad, en materiales como polietileno, polipropileno, materiales termoencogibles, entre otros.</li>
+					<li>Confeccionamos bolsas impresas o transparentes, con adhesivo, con solapa interior o exterior, solapa volada o exterior invertida, y fuelle de fondo.</li>
+					<li>Sellamos desde 3 hasta 120 cm de largo, en sello de fondo o sello lateral.</li>
+					<li>Sellamos bolsas en alta densidad con fuelles laterales hasta calibre 3.</li>
+					<li>A sus rollos planos le hacemos fuelle abierto para troquel riñón. Tambien le hacemos el sello falso con el nombre de su empresa.</li>
+					<li>Sellado para bolsas a prueba de líquidos con medidas exactas (en milímetros).</li>
+					<li>Refilamos rollos planos y con solapa.</li>
+					<li>Reembobinamos para que sus rollos den el máximo rendimiento.</li>
+				</ul>
+			</div>
+		</div>
 
-      <hr class="featurette-divider">
+		<hr class="featurette-divider">
 
-      <div class="row featurette">
-        <div class="col-md-12">
-          <h2 id="contacto" class="featurette-heading">Contacto</h2>
-          <p class="lead">Nos encontramos ubicados en el barrio Belén Miravalle, en Medellín.</p>
-          <p class="lead">Dirección: CR 79A CL 32A 50</p>
-          <p class="lead">Teléfono: 5805133</p>
-          <p class="lead">Celular: 301 650 5605 - 301 379 3711</p>
-          <p class="lead">E-mail: <a href="mailto:adyplast@gmail.com">adyplast@gmail.com</a></p>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1983.1101624455212!2d-75.60042099999995!3d6.234662000000019!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNsKwMTQnMDQuOCJOIDc1wrAzNicwMS41Ilc!5e0!3m2!1sen!2sus!4v1421119591434" width="600" height="450" frameborder="0" style="border:0"></iframe>
-        </div>
-      </div>
+		<div class="row featurette">
+			<div class="col-md-12">
+				<h2 id="productos" class="featurette-heading">Nuestros Productos</h2>
+				<p class="lead">
+					Confeccionamos bates publicitarios (proporcionamos la válvula).
+				</p>
+				<p class="lead">
+					Somos inventores de la bolsa separadora de residuos en 2 y 3 compartimientos.
+				</p>
+			</div>
+		</div>
 
-      <hr class="featurette-divider">
+		<hr class="featurette-divider">
 
-      <!-- FOOTER -->
-      <footer>
-        <p class="pull-right"><a href="#">Volver al inicio</a></p>
-        <p>&copy; 2015 Adyplast.co | Medellín </p>
-      </footer>
+		<div class="row featurette">
+			<div class="col-md-12">
+				<h2 id="contacto" class="featurette-heading">Contacto</h2>
+				<p class="lead">Nos encontramos ubicados en el barrio Belén Miravalle, en Medellín.</p>
+				<p class="lead">Dirección: CR 79A CL 32A 50</p>
+				<p class="lead">Teléfono: 5805133</p>
+				<p class="lead">Celular: 301 650 5605 - 301 379 3711</p>
+				<p class="lead">E-mail: <a href="mailto:adyplast@gmail.com">adyplast@gmail.com</a></p>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1983.1101624455212!2d-75.60042099999995!3d6.234662000000019!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNsKwMTQnMDQuOCJOIDc1wrAzNicwMS41Ilc!5e0!3m2!1sen!2sus!4v1421119591434" width="600" height="450" frameborder="0" style="border:0"></iframe>
+			</div>
+		</div>
 
-    </div><!-- /.container -->
-  </body>
+		<hr class="featurette-divider">
+
+		<div class="row featurette">
+			<div class="col-md-12">
+				<h2 id="escribenos" class="featurette-heading">Escríbenos!</h2>
+				<form>
+					<div class="form-group">
+						<label for="name">Nombre</label>
+						<input type="text" class="form-control" id="name" placeholder="Ingresa tu nombre">
+					</div>
+					<div class="form-group">
+						<label for="message">Mensaje</label>
+						<textarea class="form-control" id="message" rows="5" placeholder="Déjanos tu mensaje"></textarea>
+					</div>
+					<button type="submit" class="btn btn-default">Enviar</button>
+				</form>
+			</div>
+		</div>
+
+		<hr class="featurette-divider">
+
+		<!-- FOOTER -->
+		<footer>
+			<p class="pull-right"><a href="#">Volver al inicio</a></p>
+			<p>&copy; 2015 Adyplast.co | Medellín </p>
+		</footer>
+
+	</div><!-- /.container -->
+</body>
 </html>
